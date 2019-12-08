e6dd14c38c56f9e26286bc42074e5820407326c8
Pranish Shrestha <shresthapranish99@gmail.com>
2019-12-08 14:08:14 +1100

    minor adjustments

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Landing from "../Landing-page/Landing";
+import Landing from "../Landing-page/landing";
 
 export default function Header() {
   return (
@@ -14,33 +14,32 @@ export default function Header() {
     //     navlinks.style.display = "none";
     //   }
     // };
-    <Router>
-      <header>
-        {/* <div className="hamburger-menu" onClick={this.Handleclick}>
+
+    <header>
+      {/* <div className="hamburger-menu" onClick={this.Handleclick}>
               <span></span>
               <span></span>
               <span></span>
             </div> */}
-        <div className="logo-container">
-          <h4>E-Classmate</h4>
-        </div>
+      <div className="logo-container">
+        <h4>E-Classmate</h4>
+      </div>
 
-        <nav>
-          <ul className="nav-links">
-            <Link to="../Landing-page/Landing">
-              <a href="#">
-                <li className="light-speed-in">Contact</li>
-              </a>
-            </Link>
+      <nav>
+        <ul className="nav-links">
+          <Link to="../Landing-page/landing">
             <a href="#">
-              <li className="light-speed-in">About</li>
+              <li className="light-speed-in">Contact</li>
             </a>
-            <a href="#">
-              <li className="light-speed-in">Log in</li>
-            </a>
-          </ul>
-        </nav>
-      </header>
-    </Router>
+          </Link>
+          <a href="#">
+            <li className="light-speed-in">About</li>
+          </a>
+          <a href="#">
+            <li className="light-speed-in">Log in</li>
+          </a>
+        </ul>
+      </nav>
+    </header>
   );
 }
