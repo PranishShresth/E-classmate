24dd13eb7147ec45b4ff5760551cd22108641133
Pranish Shrestha <shresthapranish99@gmail.com>
2019-12-08 15:27:22 +1100

    new branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./App.css";
-
-import Dashboard from './pages/dashboard/dashboard';
-import Header from './components/Header/header';
-import Landing from './pages/landing/landing';
+import Dashboard from "./pages/dashboard/dashboard";
+import Header from "./components/Header/header";
+import Landing from "./pages/landing/landing";
 import Registration from "./components/Sign-up/registration";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
-      <div className="App">
-          <Header />
-          <Route path="/" exact component={Landing} />
-          <Route path="/signup" exact component={Registration} />
-          <Route path="/dashboard" component={Dashboard} />
-      </div>
+    <div className="App">
+      <Header />
+      <Route path="/" exact component={Landing} />
+      <Route path="/signup" exact component={Registration} />
+      <Route path="/dashboard" component={Dashboard} />
+    </div>
   );
 }
 
