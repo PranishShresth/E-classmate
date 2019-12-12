ea11a59184ebbd53818f3f4aa017bf1aae7605f1
Binish Joshi <beeneez6233@gmail.com>
2019-12-12 22:50:53 +0545

    Some UI

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 
-import AccountCircle from "@material-ui/icons/AccountCircle";
+import { AccountCircle } from "@material-ui/icons";
+import 
+  { Tabs,
+    Tab
+  } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+import DashboardHeader from '../../components/DashboardHeader/dashboard-header';
 import "./dashboard.css";
 
-export default class Dashboard extends React.Component {
-  render() {
-    return (
-      <div className="dasboard-container">
-        <div className="main-wrapper">
-          <div className="left-nav-bar">
-            <h1>ClassName</h1>
-            <hr />
-            <h1>Navigate</h1>
-            <ul>
-              <li>Assignments</li>
-              <li>Notes</li>
-              <li>Chats</li>
-            </ul>
-          </div>
-          <AppBar />
-        </div>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  tabs: {
+    alignContent: 'center'
   }
-}
+}));
 
-const AppBar = () => {
+const Dashboard = () => {
+  const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
   return (
-    <div class="App-bar-container">
-      <header>
-        <h1>E-Classmate</h1>
-        <AccountCircle />
-      </header>
-      <div className="contents"></div>
+    <div className="dasboard-container">
+        <DashboardHeader />
+        <Tabs className={classes.tabs} value={value} onChange={handleChange} aria-label="simple tabs example" variant="fullWidth">
+          <Tab label="ASSIGNMENT" {...a11yProps(0)} />
+          <Tab label="CHAT" {...a11yProps(1)} />
+          <Tab label="NOTE" {...a11yProps(2)} />
+      </Tabs>
     </div>
   );
-};
+}
+
+export default Dashboard;
