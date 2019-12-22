54fbb56f1f18548303cefb11293000a930b03b7c
Binish Joshi <beeneez6233@gmail.com>
2019-12-22 07:59:32 -0500

    Upvote Buttons

--- src/components/EmptyPost/EmptyPost.jsx
+++ src/components/EmptyPost/EmptyPost.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardHeader } from '@material-ui/core';
+import { Card, Typography } from '@material-ui/core';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 import UploadButtonDialog from '../../components/UploadButtonDialog/UploadButtonDialog';
 
@@ -10,16 +12,40 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: theme.palette.common.black,
         marginBottom: '3vh',
     },
+    vote: {
+        width: '21px',
+        marginRight: '3px',
+        marginLeft: '2vw',
+        marginTop: '1.5vh',
+    },
+    voteTitle: {
+        display: 'flex',
+        marginBottom: '3vh',
+    },
+    titles: {
+        marginTop: '1.5vh',
+    },
+    upvotes: {
+        marginTop: '3.7vh',
+        marginRight: '13px',
+    },
 }))
 
 const EmptyPost = () => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            <CardHeader
-                title='Maths BPB Ex 2.2'
-                subheader='Requested by @sagarmichael' 
-            />
+            <div className={classes.voteTitle}>
+                <div className={classes.vote}>
+                    <ArrowDropUpIcon />
+                    <ArrowDropDownIcon />
+                </div>
+                <Typography className={classes.upvotes}>6</Typography>
+                <div className={classes.titles}>
+                    <Typography variant='h6'>Homework</Typography>
+                    <Typography variant='subtitle2'>Requested by @username</Typography>
+                </div>
+            </div>
             <UploadButtonDialog />
         </Card>
     );
