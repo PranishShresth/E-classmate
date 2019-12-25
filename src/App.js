5e5bd4863c8fa87dca8bc17b325fd001c044963c
Binish Joshi <beeneez6233@gmail.com>
2019-12-25 18:38:27 -0500

    New Header and new Sign In Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Dashboard from "./pages/dashboard/dashboard";
 import Landing from "./pages/landing/landing";
 import Registration from "./pages/registration/registration";
+import SignIn from './pages/signin/sign-in';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Error from "./pages/404/Error";
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/signup" exact component={Registration} />
+          <Route path="/signin" exact component={SignIn} />          
           <Route path="/dashboard" exact component={Dashboard} />
           <Route exact component={Error} />
         </Switch>
