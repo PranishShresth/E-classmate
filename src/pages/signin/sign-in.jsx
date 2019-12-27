14de93143e85d4a071957322ad7ccfc6989f4654
Binish Joshi <beeneez6233@gmail.com>
2019-12-27 01:29:14 -0500

    Seperated Sign Up Form from signin page

--- src/pages/signin/sign-in.jsx
+++ src/pages/signin/sign-in.jsx
@@ -1,36 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, Button, Paper, Tab, Tabs, TextField, Typography } from '@material-ui/core';
+import { Box, Paper, Tab, Tabs, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core'
 
 import NewHeader from '../../components/NewHeader/NewHeder';
 import SignInForm from '../../components/SignIn/SignIn';
+import SignUpForm from '../../components/SignUp/SignUp';
 
 const useStyles = makeStyles(theme =>  ({
 	root: {
 		flexGrow: 1,
 		height: '90vh',
 	},
-	title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-	},
 	top: {
 		paddingTop: '14vh',
 		paddingLeft: '4vw',
-	},
-	textField: {
-		marginBottom: '10px',
-	},
-	button: {
-		marginBottom: '10px',
-		marginRight: '20vw',
-	},
-	tos: {
-		marginRight: '10vw',
 	},
 }));
 
@@ -67,22 +52,6 @@ function a11yProps(index) {
 const SignIn = () => {
 	const classes = useStyles();
 	const [value, setValue] = React.useState(0);
-	const [inputs, setInputs] = React.useState({});
-
-	const handleSubmit = event => {
-		if(event) {
-			event.preventDefault();
-		}
-		console.log(inputs);
-	};
-
-	const handleInputChange = event => {
-		event.persist();
-		setInputs(inputs => ({
-			...inputs,
-			[event.target.name]: event.target.value
-		}));
-	};
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -106,60 +75,9 @@ const SignIn = () => {
 					<TabPanel value={value} index={0}>
 						<SignInForm />
 					</TabPanel>
+
 					<TabPanel value={value} index={1}>
-						<form onSubmit={handleSubmit}>
-							<div className={classes.formContainer}>
-								<TextField 
-									className={classes.textField}
-									onChange={handleInputChange}
-									value={inputs.email}
-									label="Email"
-									name="email"
-									required
-									fullWidth
-								/>
-								<TextField 
-									className={classes.textField}
-									onChange={handleInputChange}
-									value={inputs.username}
-									label="Username"
-									name="username"
-									required
-									fullWidth
-								/>
-								<TextField 
-									className={classes.textField}
-									onChange={handleInputChange}
-									value={inputs.password}
-									label="Password"
-									name="password"
-									required
-									fullWidth
-									type="password"
-								/>
-								<TextField 
-									className={classes.textField} 
-									onChange={handleInputChange}
-									value={inputs.confirmPassword}
-									name="confirmPassword"
-									label="Confirm Password" 
-									required 
-									fullWidth 
-									type="password"
-								/>
-								<Button
-									className={classes.button}
-									type="submit"
-									variant="contained"
-									color="primary"
-								>
-									Sign Up
-								</Button>
-								<Button className={classes.button} variant="contained">
-									Sign Up with Google
-								</Button>
-							</div>
-						</form>
+						<SignUpForm />
 					</TabPanel>
 				</Paper>
 			</Paper>
